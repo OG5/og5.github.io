599c32bc744db6be87cca8af24130b55055a9c91
antisozial <58865708+antisozial@users.noreply.github.com>
2020-08-23 13:36:46 +0200

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -14,7 +14,7 @@
     $(document).ready(function () {
         var links = [
             {
-                name: 'kumi',
+                name: 'moon',
                 link: '76561199003625533'
             },
                 
